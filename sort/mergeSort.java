140f7894e650537bb55ebb2a36e146339f0df442
xzhan <zjuzhanxf@gmail.com>
2017-10-05 21:47:05 -0700

    add merge sort

--- sort/mergeSort.java
+++ sort/mergeSort.java
@@ -1,1 +1,34 @@
 /* merge sort*/
+class Solution {
+	public void mergeSort (int[] nums ) {
+		int[] aux = nums.clone();
+		mergeSort(nums, 0, nums.length-1, aux);
+	}
+
+	public void mergeSort(int[] nums, int lo, int hi, int[] aux) {
+		if (lo >= hi) return;
+		int mid = lo + (hi-lo)/2;
+		mergeSort(aux, lo, mid, nums);
+		mergeSort(aux, mid+1, hi, nums);
+		merge(nums, lo, mid, hi, aux);
+	}
+
+	public void merge(int[] nums, int lo, int mid, int hi, int[] aux) {
+		int i = lo, j = mid+1;
+		int k = lo;
+		while (i <= mid && j <= hi) {
+			if (aux[i] < aux[j]) {
+				nums[k++] = aux[i++];
+			} else {
+				nums[k++] = aux[j++];
+			}
+		}
+
+		while (i <= mid) {
+			nums[k++] = aux[i++];
+		}
+		while (j <= hi) {
+			nums[k++] = aux[j++];
+		}
+	}
+}
